356ac9c8a853b306f1fb4c2047a1c34bb916e38f
shrinath <shrinath.mirashi@byjus.com>
2023-02-09 06:39:38 +0530

    push to github

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+   async setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/features/*.features",
+    baseUrl: "https://demo.guru99.com/insurance/v1/index.php",
+    chromeWebSecurity: true,
   },
 });
